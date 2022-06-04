9c4b0db847b7ca46959621381cc16b7c12adf179
karankumarshreds <naileditnerd@gmail.com>
2022-06-04 15:02:14 +0530

    update common package

--- go-broker/cmd/api/handlers.go
+++ go-broker/cmd/api/handlers.go
@@ -3,15 +3,16 @@ package main
 import (
 	"log"
 	"net/http"
+	common "github.com/karankumarshreds/GoMicroservices/go-common/http"
 )
 
 func (a *App) BrokerHandler(w http.ResponseWriter, r *http.Request) {
-	payload := JsonResponse{
+	payload := common.JsonResponse{
 		Error:   false,
 		Message: "Hit the broker",
 	}
 
-	err := a.WriteJSON(w, payload, http.StatusOK)
+	err := common.WriteJSON(w, payload, http.StatusOK)
 	if err != nil {
 		log.Println("Unable to send response to the client", err)
 	}
--- go-broker/go.mod
+++ go-broker/go.mod
@@ -3,6 +3,7 @@ module github.com/karankumarshreds/GoMicroservices/go-broker
 go 1.16
 
 require (
-	github.com/go-chi/chi/v5 v5.0.7 // indirect
-	github.com/go-chi/cors v1.2.1 // indirect
+	github.com/go-chi/chi/v5 v5.0.7
+	github.com/go-chi/cors v1.2.1
+	github.com/karankumarshreds/GoMicroservices v0.0.0-20220604092804-0c9bbbeb3bc4
 )
